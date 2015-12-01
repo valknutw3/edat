29aca26c4d0c4362df97ad9bc9412964d15c648e
davidnevadoc <nevadod@gmx.com>
2015-12-01 19:03:38 +0100

    add push script 2

--- push.sh
+++ push.sh
@@ -3,5 +3,5 @@
 
 echo "Push all commited changes into davidnevadoc/PPROG.git"
 
-git remote add origin https://github.com/davidnevadoc/PPROG.git
+git remote add origin https://github.com/valknutw3/edat.git
 git push origin master
